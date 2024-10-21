060a7a497a347470258e3f5ac619abb59ed852ee
Rishi Vyas <rishi.vyas1110@gamil.com>
2024-10-21 13:14:21 +0530

    change wsig settings

--- testing/wsgi.py
+++ testing/wsgi.py
@@ -7,20 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-
 import os
-import sys
 
-# add your project directory to the sys.path
-project_home = '/home/RishiVyas/MultimodelAi'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-# set environment variable to tell django where your settings.py is
-os.environ['DJANGO_SETTINGS_MODULE'] = 'MultimodelAi.settings'
-
-
-# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testing.settings")
+
+application = get_wsgi_application()
