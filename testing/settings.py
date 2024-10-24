d35835df13e92925238ee15d4c0768dc07218f8a
Rishi Vyas <rishi.vyas1110@gamil.com>
2024-10-24 09:58:24 +0530

    upgrad the env file

--- testing/settings.py
+++ testing/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-*)lo=a6bi@(cmjwu)$qhwvqnu(mm50@k^9-cl9m*=d(aswb2au"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
